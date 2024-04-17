29beb47fa889d85f1b400cb37311c0bbb4be3c30
SHELARARNAVI <124708886+SHELARARNAVI@users.noreply.github.com>
2024-04-17 08:46:14 +0530

    Add files via upload

---
+++ Assignment 1/Client.java
@@ -0,0 +1,38 @@
+import java.rmi.*;
+import java.util.Scanner;
+
+
+
+public class Client{
+         public static void main(String[] args){
+                Scanner sc= new Scanner(System.in);
+                
+                try{
+                        String serverURL = "rmi://localhost/Server";
+                        ServerIntf serverIntf = (ServerIntf) Naming .lookup(serverURL);
+                        
+                        System.out.print("Enter First Number: ");
+                        double num1 = sc.nextDouble();
+                        
+                        System.out.print("Enter Second Number: ");
+                        double num2 = sc.nextDouble();
+                        
+                        
+                        System.out.println("First Number Is: " + num1);
+                        System.out.println("Second Number Is: " + num2);
+                        
+                        
+                        System.out.println("-------------Results-------------");
+                        System.out.println("Addition Is: " +serverIntf.Addition(num1, num2));
+                        System.out.println("Addition Is: " +serverIntf.Subtraction(num1, num2));
+                        System.out.println("Addition Is: " +serverIntf.Multiplication(num1, num2));
+                        System.out.println("Addition Is: " +serverIntf.Division(num1, num2));
+                        
+                        
+                }catch(Exception e){
+                        System.out.println("Exception Occured At Client!" + e.getMessage());
+                }
+        
+        }
+        
+}
